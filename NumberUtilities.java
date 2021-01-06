373b4107941518f003a6b0174a305fca049fb64e
'nortonlyr' <'nortonlyr@gmail.com'>
2021-01-06 14:06:41 -0500

    NumberUtilities class updated

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -4,29 +4,46 @@
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+        return getRange(0, stop);
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        return getRange(start, stop, 1);
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String expected = "";
+        for (int i=start; i<stop; i+=step) {
+        	expected += 1;
+        }
+        
+        return expected;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        if (start%2==0) {
+        	return getRange(start, stop, 2);
+        }else {
+        	return getRange(start+1, stop, 2);
+        }
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+    	if (start%2==0) {
+        	return getRange(start+1, stop, 2);
+        }else {
+        	return getRange(start, stop, 2);
+        }
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        String expected = "";
+        for (int i=start; i<=stop; i++) {
+        	expected += String.format("%1$.0f", Math.pow(i,  exponent));
+        }
+        return expected;
     }
 }
